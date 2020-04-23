a50657364bad2238d835da11267c037b6d06b88c
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-04-23 15:40:18 -0400

    colab

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,8 @@
 class Transfer
   # your code here
-  attr_accessor :sender, :receiver, :amount, :status
+
+  attr_accessor :sender, :receiver, :status
+  attr_reader :amount
   def initialize(sender, receiver, amount, status = "pending")
     @sender = sender
     @receiver = receiver
@@ -9,5 +11,28 @@ class Transfer
   end
 
   def valid?
+    @sender.valid? && @receiver.valid?
   end
+
+  def execute_transaction
+    if @sender.balance > @amount && @sender.valid? && @status == 'pending' && @receiver.status == 'open'
+      @sender.balance -= @amount
+      @receiver.deposit(@amount)
+      @status = 'complete'
+    else
+       @status = 'rejected'
+      p "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+      if @status == 'complete'
+        @receiver.balance -= @amount
+        @sender.balance += @amount
+        @status = 'reversed'
+      end
+
+  end
+
+
 end
