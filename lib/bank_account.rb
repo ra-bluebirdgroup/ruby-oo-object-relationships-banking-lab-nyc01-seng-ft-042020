dc2d86af1e1061d02d3cfd8ab7d633bad0dd98c2
Srishti Nath <srishti.nath@gmail.com>
2020-04-23 15:00:46 -0400

    first stab

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,25 @@
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+    def initialize(name = nil, balance = 1000, status = "open")
+        @name = name
+        @balance = balance
+        @status = status
+    end
 
+    def deposit(deposit)
+        @balance += deposit
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        @status == "open" && @balance > 0
+    end
+
+    def close_account
+        @status = "closed"
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,13 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  def initialize(sender, receiver, amount, status = "pending")
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = status
+  end
+
+  def valid?
+  end
 end
